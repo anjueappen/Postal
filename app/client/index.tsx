5ab2d9837f43e1c69e961a6a6cb24e31222a7584
Anju Eappen <aeappen@ualberta.ca>
2017-05-11 15:58:36 -0600

    Infra: Using Meteor recommended folder structue

--- app/client/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-import { Hello } from "../components/Hello";
-
-Meteor.startup(() => ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("root")
-));
