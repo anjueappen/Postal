d605e76b94755a57531f6ac2e5f682dba01499d2
Anju Eappen <aeappen@ualberta.ca>
2017-05-18 15:31:47 -0600

    Added react router

--- app/typings/index.d.ts
+++ app/typings/index.d.ts
@@ -2,4 +2,5 @@
 /// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="globals/meteor/index.d.ts" />
 /// <reference path="modules/react-dom/index.d.ts" />
+/// <reference path="modules/react-router/index.d.ts" />
 /// <reference path="modules/react/index.d.ts" />
